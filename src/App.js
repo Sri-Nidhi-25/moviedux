c9f0814c703c0f712cb30cdb81729658d02fd5b3
Sri-Nidhi-25 <140502551+Sri-Nidhi-25@users.noreply.github.com>
2024-11-14 12:49:45 +0530

    modifying app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1> Welcome to Moviedux</h1>
+      </header>
+      <footer className='footer'>
+        <p>Footer content</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
